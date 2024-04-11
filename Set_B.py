81083f835b8cf2cbcadc4fe03c3ab5a3b8b5db90
France Raphael RIvera <francerivera1231@gmail.com>
2024-04-11 21:05:35 +0800

    Update Set_B.py

--- Set_B.py
+++ Set_B.py
@@ -6,17 +6,30 @@ root.title("Tkinter Exam")
 root.geometry("500x400")
 root.resizable(False, False)
 
-
 def greeting():
     messagebox.showinfo(message=f"Hello, {entry.get()}")
 
-def font_size():
-    if selected.get() == 1:
+def change_font():
+    if c1.get() == 1:
+        entry.config(font=("Arial", 4, "normal"))
+    elif c1.get() == 2:
         entry.config(font=("Arial", 8, "normal"))
-    elif selected.get() == 2:
-        entry.config(font=("Arial", 10, "normal"))
-    elif selected.get() == 3:
+    elif c1.get() == 3:
         entry.config(font=("Arial", 12, "normal"))
+
+def add_items():
+    listbox.insert(END, entry.get())
+
+
+def about_info():
+    messagebox.showinfo("About My Program", "Features:\n\n"
+                        "You can personalize your greeting.\n"
+                        "You can customize the size of the font.\n"
+                        "You can add items on the list.\n\n"
+                        "Made by: France Rivera")
+
+
+
 
 label = Label(root, text="Enter your name")
 label.grid(row=0, column=0)
@@ -30,15 +43,37 @@ button.grid(row=1, column=1)
 frame = Frame(root)
 frame.grid(row=3, column=1)
 
-selected = IntVar()
-op1 = Radiobutton(frame, text="Small", value=1, variable=selected, command=font_size)
+c1 = IntVar()
+op1 = Radiobutton(frame, text="Small", variable=c1, value=1, command=change_font)
 op1.pack(anchor=W)
-
-op2 = Radiobutton(frame, text="Medium", value=2, variable=selected, command=font_size)
+op2 = Radiobutton(frame, text="Medium", variable=c1, value=2, command=change_font)
 op2.pack(anchor=W)
-
-op3 = Radiobutton(frame, text="Large", value=3, variable=selected, command=font_size)
+op3 = Radiobutton(frame, text="Large", variable=c1, value=3, command=change_font)
 op3.pack(anchor=W)
+
+
+scrollbar = Scrollbar(frame)
+scrollbar.pack(side=RIGHT, expand=True, fill=Y)
+
+listbox = Listbox(frame, yscrollcommand=scrollbar.set, width=20, height=10)
+listbox.pack()
+listbox.insert(END, "Emilio Aguinaldo")
+listbox.insert(END, "Ferdinand Marcos")
+listbox.insert(END, "Rodrigo Duterte")
+listbox.insert(END, "Gloria Arroyo")
+listbox.insert(END, "Emilio Aguinaldo")
+
+menubar = Menu(root)
+
+new = Menu(menubar, tearoff=0)
+menubar.add_command(label="New", command=add_items)
+
+
+about = Menu(menubar, tearoff=0)
+menubar.add_command(label="About", command=about_info)
+
+
+root.config(menu=menubar)
 
 
 root.mainloop()
